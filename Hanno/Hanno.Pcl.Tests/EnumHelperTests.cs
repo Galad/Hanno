e537216d186d3fa1b098c155f857ec5e307cc552
Lucas Loegel <lucas.loegel@outlook.com>
2014-08-23 15:05:23 -0400

    Add EnumHelper

---
+++ Hanno/Hanno.Pcl.Tests/EnumHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using Ploeh.AutoFixture.Xunit;
+using Ploeh.AutoFixture;
+using Xunit.Extensions;
+using Xunit;
+using FluentAssertions;
+using Moq;
+
+namespace Hanno.Tests
+{
+	public class EnumHelperTests
+	{
+		public enum EnumTest
+		{
+			Value1,
+			Value2,
+			Value3
+		}
+
+		[Fact]
+		public void GetValues_ShouldReturnCorrectValues()
+		{
+			//act
+			var actual = EnumHelper.GetValues<EnumTest>();
+
+			var expected = new EnumTest[] {EnumTest.Value1, EnumTest.Value2, EnumTest.Value3};
+			actual.ShouldAllBeEquivalentTo(expected);
+		}
+	}
+}
---
+++ Hanno/Hanno.Pcl/EnumHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hanno
+{
+	public static class EnumHelper
+	{
+		public static IEnumerable<T> GetValues<T>() where T : struct
+		{
+			return Enum.GetValues(typeof(T))
+					   .Cast<T>();
+		}
+	}
+}
